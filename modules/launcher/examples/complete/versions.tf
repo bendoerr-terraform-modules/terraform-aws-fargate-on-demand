ecd623a1168591cabeb41aec2384282527c84d2e
Ben Doerr <craftsman@bendoerr.me>
2023-09-05 20:24:06 -0400

    🚨 (lint): Addresses lint and tfsec issues

--- modules/launcher/examples/complete/versions.tf
+++ modules/launcher/examples/complete/versions.tf
@@ -10,5 +10,5 @@ terraform {
 
 # AWS Provider Configuration
 provider "aws" {
-  region              = "us-east-1"
+  region = "us-east-1"
 }
--- modules/persistence/outputs.tf
+++ modules/persistence/outputs.tf
@@ -1,29 +1,29 @@
 output "name" {
-  value = module.label_data.id
+  value       = module.label_data.id
   description = "Name of the EFS file system."
 }
 
 output "file_system_id" {
-  value = aws_efs_file_system.data.id
+  value       = aws_efs_file_system.data.id
   description = "EFS file system ID."
 }
 
 output "access_point_id" {
-  value = aws_efs_access_point.data.id
+  value       = aws_efs_access_point.data.id
   description = "EFS access point ID."
 }
 
 output "mount_path" {
-  value = var.mount_path
+  value       = var.mount_path
   description = "EFS mount point."
 }
 
 output "access_policy_arn" {
-  value = aws_iam_policy.data_rw.arn
+  value       = aws_iam_policy.data_rw.arn
   description = "IAM policy that allows mounting and writing to the file system via the access point."
 }
 
 output "access_security_group" {
-  value = aws_security_group.data_nfs.id
+  value       = aws_security_group.data_nfs.id
   description = "Security Group that allows accessing the file system via NFS mount point."
 }
