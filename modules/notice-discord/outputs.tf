ff6c0eea899706c61174b3774fa9d40e8f53fca6
Ben Doerr <craftsman@bendoerr.me>
2023-12-02 23:46:11 -0500

    🍱 (notice-discord): Adds notice_role_name output (#87)

---
+++ modules/notice-discord/outputs.tf
@@ -0,0 +1,9 @@
+output "notice_role_name" {
+  value       = aws_iam_role.notice_role.name
+  description = "TODO"
+}
+
+output "notice_role_arn" {
+  value       = aws_iam_role.notice_role.arn
+  description = "TODO"
+}
